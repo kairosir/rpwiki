bbaf6ac75f856d220a6338d615f942ca328b8e93
kairosir <44501534+kairosir@users.noreply.github.com>
2025-04-12 00:41:57 +0500

    Обновление конфигурации базы данных: использование переменной окружения DATABASE_URL для подключения к базе данных, что упрощает настройку и улучшает гибкость конфигурации.

--- strapi-cms/config/database.ts
+++ strapi-cms/config/database.ts
@@ -3,7 +3,7 @@ import { DatabaseConfig } from '@strapi/strapi';
 const config: DatabaseConfig = {
   connection: {
     client: 'postgres',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT),
       database: process.env.DATABASE_NAME,
@@ -11,10 +11,9 @@ const config: DatabaseConfig = {
       password: process.env.DATABASE_PASSWORD,
       ssl: {
         rejectUnauthorized: false
-      },
-    },
-    debug: false,
-  },
+      }
+    }
+  }
 };
 
 export default config;
