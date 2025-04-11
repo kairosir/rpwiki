98a58e692209276ecccdde464dc2f82973ddbe2b
kairosir <44501534+kairosir@users.noreply.github.com>
2025-04-12 00:45:51 +0500

    Refactor database configuration: implement conditional connection setup using DATABASE_URL for improved flexibility and maintainability.

--- strapi-cms/config/database.ts
+++ strapi-cms/config/database.ts
@@ -1,18 +1,34 @@
 import { DatabaseConfig } from '@strapi/strapi';
+import { parse } from 'pg-connection-string';
 
 const config: DatabaseConfig = {
   connection: {
     client: 'postgres',
-    connection: process.env.DATABASE_URL || {
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      ssl: {
-        rejectUnauthorized: false
+    connection: (() => {
+      if (process.env.DATABASE_URL) {
+        const parsed = parse(process.env.DATABASE_URL);
+        return {
+          host: parsed.host,
+          port: Number(parsed.port),
+          database: parsed.database,
+          user: parsed.user,
+          password: parsed.password,
+          ssl: {
+            rejectUnauthorized: false
+          }
+        };
       }
-    }
+      return {
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      };
+    })()
   }
 };
 
