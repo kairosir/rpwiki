af8c121eba44e1a4ad5a4337471e5360ca753d1a
kairosir <44501534+kairosir@users.noreply.github.com>
2025-04-13 01:29:21 +0500

    Enhance database configuration: set default values for connection parameters and simplify SSL settings for improved clarity and maintainability.

--- strapi-cms/config/database.ts
+++ strapi-cms/config/database.ts
@@ -4,14 +4,18 @@ const config: DatabaseConfig = {
   connection: {
     client: 'postgres',
     connection: {
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
+      host: process.env.DATABASE_HOST || 'db.wlpawjhyybiwylghxnhw.supabase.co',
+      port: Number(process.env.DATABASE_PORT) || 5432,
+      database: process.env.DATABASE_NAME || 'postgres',
+      user: process.env.DATABASE_USERNAME || 'postgres',
       password: process.env.DATABASE_PASSWORD,
-      ssl: process.env.DATABASE_SSL ? {
-        rejectUnauthorized: process.env.DATABASE_SSL_REJECT_UNAUTHORIZED === 'false' ? false : true,
-      } : false,
+      ssl: {
+        rejectUnauthorized: false
+      },
+      pool: {
+        min: 0,
+        max: 5
+      }
     },
     debug: false,
   },
